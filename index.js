dd7badad16f3a0ce764f9c98b1057554bd70c33f
dhef415 <dhef415@gmail.com>
2020-12-10 22:44:56 -0800

    T6

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ export const burger = {
   category: "Lunch",
   discount(string){
     if (string==="teacher"){
-      return `${this.price}`-1/4;
+      return `${this.price}`- 4.5;
     }else if(string==="student"){
-      return `${this.price}`-1/10;
+      return `${this.price}`-1.8;
     }else{
       return `${this.price}`
     }
@@ -89,7 +89,7 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-console.log(reviews.push("Daniel",5,"Everything was great."))
+console.log(reviews.push("Daniel",5,"Everything was great."));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -113,8 +113,8 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(array,num) {
+  return array[num];
 }
 
 
